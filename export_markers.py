e29c7f28e80c3ef03f3b8396703a335b1e34cc16
César Augusto Noronha e Sousa Filho <cesar.noronha@idor.org>
2021-11-25 12:33:04 -0300

    second commit

--- export_markers.py
+++ export_markers.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Adjust here
-filename = '2021-11-18_001_lsl.tri'
+filename = 'Desktop/NIRS_triggers/2021-11-12_001_lsl.tri'
 interval = 3
 condition = 2
 outname = 'out.txt'
@@ -11,6 +11,7 @@ df = pd.read_csv(filename, header=None, delimiter=';')
 
 df_filtered = df[df[2] == condition] # Filter rows of an certain condition
 sel_rows = list(range(0, len(df_filtered), interval)) # Selected rows
+df_filtered.reset_index(inplace=True)
 final_markers = df_filtered.loc[sel_rows]
 
 with open(outname, 'w') as f:
